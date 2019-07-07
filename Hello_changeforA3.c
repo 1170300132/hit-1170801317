a2a7faf2006ad05e7098a8e89a13e47b50b63e70
1170300132 <48054475+1170300132@users.noreply.github.com>
2019-07-07 19:46:09 +0800

    Update Hello_changeforA3.c

--- Hello_changeforA3.c
+++ Hello_changeforA3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(void) {
-	printf("Hello World!There is a change for A3 from hit-1170300132.");
+	printf("Hello World!There is another change for A3 from hit-1170300132.");
   return 0;
 }
